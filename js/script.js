45831c2e2182325c63494b9670d2cd45cfefc753
Andrey <azbangwtf@ya.ru>
2022-01-07 14:34:12 +0300

    new design

--- js/script.js
+++ js/script.js
@@ -1,49 +1,58 @@
+window.addEventListener("load", function () {
+  function parseData(url) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.send();
+    if (xhr.status == 200) {
+      var totalSpendings = Math.floor(
+        JSON.parse(xhr.responseText)["oops_total"]
+      );
+      return totalSpendings;
+    } else console.log(xhr.status + " " + xhr.satusText);
+  }
 
-window.addEventListener("load",function(){
-	function parseData(url){
-		let xhr = new XMLHttpRequest();
-		xhr.open('GET', url, false);
-		xhr.send();
-		if (xhr.status == 200){
-			var totalSpendings = Math.floor(JSON.parse(xhr.responseText)['oops_total']);
-			return(totalSpendings);
-		} else
-			console.log(xhr.status + ' ' + xhr.satusText);
-	}
+  function increaseValue(increase, currentValue) {
+    let needTime = increase / 10;
+    increasedValue = currentValue + increase;
+    const countDown = setInterval(() => {
+      currentValue++;
+      money.innerHTML = currentValue.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+        maximumFractionDigits: 0,
+      });
+      if (increasedValue - currentValue == 0) clearInterval(countDown);
+    }, (1000 * needTime) / increase);
+    return increasedValue;
+  }
 
-	function increaseValue(increase, currentValue){
-		let needTime = increase / 10;
-		increasedValue = currentValue + increase;
-		const countDown = setInterval(()=>{
-			currentValue++;
-			money.innerHTML = currentValue.toLocaleString("en-US", { 
-			    style: "currency", 
-			    currency: "USD",
-			    maximumFractionDigits: 0
-		  	});
-			if ((increasedValue) - currentValue == 0)
-				clearInterval(countDown);
-		}, 1000 * needTime / increase);
-		return (increasedValue);
-	}
+  function checkNew(url, currentValue) {
+    let newValue = parseData(url) + 100;
+    if (newValue != currentValue) increaseValue(newValue - currentValue);
+  }
 
-	function checkNew(url, currentValue){
-		let newValue = parseData(url) + 100;
-		if (newValue != currentValue)
-			increaseValue(newValue - currentValue);
-		
-	}
+  var phoneInput = document.getElementById("whitelist-phone");
+  var phoneButton = document.getElementById("whitelist-submit");
+  var phoneMask = IMask(phoneInput, {
+    mask: "+{1} (000) 000-000",
+  });
 
-	const money = document.querySelector(".money");
-	let url = 'https://api.oops.finance/api/v1/public/oops_total';
-	let value = parseInt(parseData(url)); //parse initial value
-	money.innerHTML = value.toLocaleString("en-US", { 
-		style: "currency", 
-		currency: "USD",
-		maximumFractionDigits: 0
-	});
-	let difference = 100; // initial difference in money to make nice animation
+  phoneButton.disabled = !phoneMask.masked.isComplete;
+  phoneMask.on("accept", function () {
+    phoneButton.disabled = !phoneMask.masked.isComplete;
+    // TODO Back
+  });
 
-	value = increaseValue(difference, value);
-	setInterval(checkNew, 60000, url, value); // change the time when it parses new data
-})
+  const money = document.querySelector(".main__total");
+  let url = "https://api.oops.finance/api/v1/public/oops_total";
+  let value = parseInt(parseData(url)); //parse initial value
+  money.innerHTML = value.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 0,
+  });
+  let difference = 100; // initial difference in money to make nice animation
+
+  value = increaseValue(difference, value);
+  setInterval(checkNew, 60000, url, value); // change the time when it parses new data
+});
